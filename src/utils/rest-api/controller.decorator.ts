81048d5e3e6d57839041d749f4a4448a0f57abf6
kulcsarrudolf <kulcsarrudolf@gmail.com>
2025-02-17 20:16:31 +0200

    Removed Unused Code

--- src/utils/rest-api/controller.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { controllers } from './metadata';
-
-export function Controller(path: string): ClassDecorator {
-  return (target) => {
-    controllers.push({ path, target });
-  };
-}
--- src/utils/rest-api/index.ts
+++
@@ -1,4 +0,0 @@
-export { Controller } from './controller.decorator';
-export { Get, Post, Put, Delete } from './routes.decorator';
-export { Middleware } from './middleware.decorator';
-export { registerRoutes } from './register-routes';
